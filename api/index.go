10da4bbfe5603df2305c6238a6651a3b3dab146b
Thanaphon <voigkgky@gmail.com>
2026-05-17 11:56:20 +0700

    first commit

--- api/index.go
+++ api/index.go
@@ -1,1 +1,100 @@
 package handler
+
+import (
+	"net/http"
+	"os"
+
+	"music/adapter"
+	"music/service"
+	"music/utils"
+
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var app *fiber.App
+
+func init() {
+	dsn := os.Getenv("HDAtABACE")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	app = fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "https://song-forns.vercel.app",
+		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+	}))
+
+	//User
+	adapterdb := adapter.NewGormUserDB(db)
+	serveruser := service.NewUserService(adapterdb)
+	adapterhttp := adapter.NewUserHttps(serveruser)
+
+	app.Post("/user", adapterhttp.CreatUser)
+	app.Post("/login", adapterhttp.Login)
+	app.Get("/me", utils.AuthRequired, adapterhttp.ShowUsserID)
+
+	//แสดงรูปภาพ
+	app.Get("/images/:name", utils.AuthRequired, adapterhttp.ImageShow)
+	// app.Static("/images", "./uploads/images")
+
+	//Artist
+	artistdb := adapter.NewArtistGormDB(db)
+	svArtist := service.NewArtistService(artistdb)
+	artisthttps := adapter.NewArtistHttps(svArtist)
+
+	app.Use("/artist", utils.AuthRequired)
+	app.Post("/artist", artisthttps.SaveArtist)
+	app.Get("/artist/:id", artisthttps.ShowArtist)
+	app.Get("/artist", artisthttps.ShowArtistAll)
+	app.Get("/artists/:name", utils.AuthRequired, artisthttps.ArtistsImage)
+
+	//Song
+	songdb := adapter.NewSongGormDB(db)
+	svSong := service.NewSongService(songdb)
+	songehttps := adapter.NewSongHttps(svSong)
+
+	app.Use("/song", utils.AuthRequired)
+	app.Post("/song", songehttps.SaveSong)
+	app.Get("/song", songehttps.ShowSong)
+	app.Get("/songnew", songehttps.ShowNew)
+	app.Get("/song/play/:id", songehttps.PlaySongNow)
+	app.Get("/music/:id", songehttps.PlaySong)
+	app.Get("/song/:id/:slug", songehttps.GetSongBySlug)
+	app.Get("/cover/:name", utils.AuthRequired, songehttps.CoverImage)
+
+	//SongView
+	viewDB := adapter.NewSongViewGormDB(db)
+	svview := service.NewSongViewService(viewDB)
+	viewHandler := adapter.NewSonViewgHttps(svview)
+
+	app.Use("/history", utils.AuthRequired)
+	app.Post("/history", utils.AuthRequired, viewHandler.Create)
+	app.Get("/history", utils.AuthRequired, viewHandler.GetMyHistory)
+	app.Get("/songview", utils.AuthRequired, viewHandler.GetMyHistoryAll)
+	app.Delete("/history/:id", utils.AuthRequired, viewHandler.DeleteSongView)
+
+	//UserLibrary
+	userlibraryDB := adapter.NewLibraryGormDB(db)
+	svuserlibrary := service.NewuserLibraryService(userlibraryDB)
+	userlibraryHandler := adapter.NewuserLibraryHttps(svuserlibrary)
+
+	app.Use("/library", utils.AuthRequired)
+	app.Post("/library", userlibraryHandler.SaveLibrary)
+	app.Delete("/library/:sonId", userlibraryHandler.DeleteUserLibrary)
+	app.Get("/library/:songId", userlibraryHandler.ShowSong)
+
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	adaptor.FiberApp(app)(w, r)
+}
--- go.mod
+++ go.mod
@@ -17,7 +17,8 @@ require (
 	github.com/go-playground/validator/v10 v10.30.2 // indirect
 	github.com/goccy/go-json v0.10.6 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
-	github.com/gofiber/fiber/v2 v2.52.12 // indirect
+	github.com/gofiber/adaptor/v2 v2.2.1 // indirect
+	github.com/gofiber/fiber/v2 v2.52.13 // indirect
 	github.com/gofiber/jwt/v2 v2.2.7 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
@@ -30,11 +31,11 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.18.5 // indirect
+	github.com/klauspost/compress v1.18.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.21 // indirect
+	github.com/mattn/go-isatty v0.0.22 // indirect
 	github.com/mattn/go-runewidth v0.0.23 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -51,14 +52,14 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.70.0 // indirect
+	github.com/valyala/fasthttp v1.71.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.1 // indirect
 	golang.org/x/arch v0.26.0 // indirect
 	golang.org/x/crypto v0.50.0 // indirect
 	golang.org/x/net v0.53.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
-	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/sys v0.44.0 // indirect
 	golang.org/x/text v0.36.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gorm.io/driver/postgres v1.6.0 // indirect
